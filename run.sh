a9b6e61bb515074a5972ed3f3f0e899b196a3f70
psquickitjayant <jayant.varshney@psquickit.com>
2014-10-14 18:19:18 +0530

    run.sh: Removed syslogd configuration

--- run.sh
+++ run.sh
@@ -13,16 +13,4 @@ fi
 sed -i "s/TOKEN/$TOKEN/" /etc/rsyslog.d/50-default.conf
 sed -i "s/TAG/$TAG/" /etc/rsyslog.d/50-default.conf
 
-
-if [ -f "/usr/sbin/rsyslogd" ]; then
-	
-	#using rsyslogd if rsyslogd folder is present
-	exec rsyslogd -n
-elif [-f "/usr/sbin/syslogd" ]; then
-	
-	#using syslogd if syslogd folder is present
-	exec syslogd -n
-else
-	echo "Unable to locate syslog"
-	exit 1
-fi
+exec rsyslogd -n
