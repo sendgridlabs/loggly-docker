04a3c1524a4f90e856ef7b67543090a5b5853134
Vlad Ghinea <vlad@ghn.me>
2015-08-14 10:50:51 -0500

    Use specific environment variables
    Signed-off-by: Vlad Ghinea <vlad@ghn.me>

--- run.sh
+++ run.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-if [ -z "$TOKEN" ]; then
-  echo "Missing \$TOKEN"
+if [ -z "$LOGGLY_AUTH_TOKEN" ]; then
+  echo "Missing \$LOGGLY_AUTH_TOKEN"
   exit 1
 fi
 
-if [ -z "$TAG" ]; then
-  echo "Missing \$TAG"
+if [ -z "$LOGGLY_TAG" ]; then
+  echo "Missing \$LOGGLY_TAG"
   exit 1
 fi
 
-sed -i "s/TOKEN/$TOKEN/" /etc/rsyslog.conf
-sed -i "s/TAG/$TAG/" /etc/rsyslog.conf
+sed -i "s/LOGGLY_AUTH_TOKEN/$LOGGLY_AUTH_TOKEN/" /etc/rsyslog.conf
+sed -i "s/LOGGLY_TAG/$LOGGLY_TAG/" /etc/rsyslog.conf
 
 exec /usr/sbin/rsyslogd -n
 
