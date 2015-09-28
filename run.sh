0d12a52fa5c906f4814f6cf4dbd228da664f7c6c
Jonathan Short <jonathan@ele.uri.edu>
2015-09-28 11:51:26 -0400

    accept original env var forms as well as new

--- run.sh
+++ run.sh
@@ -2,14 +2,24 @@
 
 # Fail if LOGGLY_AUTH_TOKEN is not set
 if [ -z "$LOGGLY_AUTH_TOKEN" ]; then
-  echo "Missing \$LOGGLY_AUTH_TOKEN"
-  exit 1
+  if [ ! -z "$TOKEN" ]; then
+    # grandfather old env var
+    export LOGGLY_AUTH_TOKEN=$TOKEN
+  else
+    echo "Missing \$LOGGLY_AUTH_TOKEN"
+    exit 1
+  fi
 fi
 
 # Fail if LOGGLY_TAG is not set
 if [ -z "$LOGGLY_TAG" ]; then
-  echo "Missing \$LOGGLY_TAG"
-  exit 1
+  if [ ! -z "$TAG" ]; then
+    # grandfather old env var
+    export LOGGLY_TAG=$TAG
+  else
+    echo "Missing \$LOGGLY_TAG"
+    exit 1
+  fi
 fi
 
 # Create spool directory
